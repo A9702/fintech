f9fffd30d3f5a025d9f0f5129ad446a313de201b
luimolgar <48240015+luimolgar@users.noreply.github.com>
2019-04-25 09:00:11 +0200

    comentarios

--- js/js1.js
+++ js/js1.js
@@ -1,18 +1,19 @@
-/* Javascript Slideshow */
+/* Javascript de Slideshow */
 
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
+// Controles de siguiente imagen y anterior.
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
+// Controles imagenes
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
-
+// Funcion que controla que ocurre cuando se acaban las imagenes, que pasa a la primera de nuevo.
+//Tanto de los circulos como de los cursores anterior y siguiente
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("misimagenes");
@@ -22,27 +23,12 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
   }
+    // Añade resalto sobre el circulo correspondiente a la imagen activa
   for (i = 0; i < dots.length; i++) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
+    // block para que ocupe la linea entera (elemento de bloque)
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
-/* Javascript Acordeon*/
-var acc = document.getElementsByClassName("acordeon");
-var i;
 
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    /* Entre la activaciçon y desactivacion */
-    this.classList.toggle("active");
-
-    /* Toggle between hiding and showing the active panel */
-    var panel = this.nextElementSibling;
-    if (panel.style.display === "block") {
-      panel.style.display = "none";
-    } else {
-      panel.style.display = "block";
-    }
-  });
-}
